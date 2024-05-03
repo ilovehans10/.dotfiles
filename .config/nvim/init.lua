fde10c25424fb718bb32f9917b8aace56fa6dbfa
Hans Larsson <hanandlia@gmail.com>
2024-05-03 09:03:23 -0500

    add tmux navigation plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -156,6 +156,23 @@ require("lazy").setup({
         end
     },
     {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+    {
         "easymotion/vim-easymotion", -- easymotion allows for smarter movement
     },
     {
