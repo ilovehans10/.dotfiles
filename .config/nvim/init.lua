c33c1cd2f3f09f35d4d68b2c94864a9eb0c86c36
Hans Larsson <hanandlia@gmail.com>
2024-08-20 08:40:25 -0500

    add vim binding to edit tips file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -81,6 +81,7 @@ keyset("v", "<leader>w", "g<C-g>") -- show word count for visual selection
 keyset("n", "<leader>ev", (":tab drop " .. vim.fn.expand('<sfile>:p') .. "<CR>"))
 keyset("n", "<leader>ez", "<CMD>tab drop ~/.config/zsh/.zshrc<CR>")
 keyset("n", "<leader>ep", "<CMD>tab drop ~/.config/zsh/.zprofile<CR>")
+keyset("n", "<leader>et", "<CMD>tab drop ~/.config/zsh/tips<CR>")
 keyset("n", "<leader>s", ":s///g<Left><Left>")
 keyset("n", "<leader>S", ":%s///g<Left><Left>")
 keyset("n", "<leader>f", ":%! rustfmt<CR>")
