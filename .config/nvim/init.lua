b52842fdd77507ec5a2abf44246ad57ca029a6d5
Hans Larsson <hanandlia@gmail.com>
2025-07-23 20:11:12 -0500

    remove comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -493,10 +493,6 @@ require("lazy").setup({
 				},
 			})
 
-			-- LSP servers and clients are able to communicate to each other what features they support.
-			--  By default, Neovim doesn't support everything that is in the LSP specification.
-			--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-			--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 			-- Enable the following language servers
